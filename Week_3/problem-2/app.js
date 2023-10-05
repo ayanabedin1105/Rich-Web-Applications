3b681911519867c7e06ca1a20e6cd3f4156359c0
Ayan Abedin <65453428+ayanabedin1105@users.noreply.github.com>
2023-10-06 00:44:31 +0100

    Delete function working

--- Week_3/problem-2/app.js
+++ Week_3/problem-2/app.js
@@ -1,3 +1,20 @@
 let notesContainer = document.querySelector(".notes-container");
 let createBtn = document.querySelector(".notes-btn");
-let notes = document.querySelector(".input-box");
+let notes = document.querySelectorAll(".input-box");
+
+// creating button
+createBtn.addEventListener("click", () => {
+  let inputBox = document.createElement("p");
+  let img = document.createElement("img");
+  inputBox.className = "input-box";
+  inputBox.setAttribute("contenteditable", "true");
+  img.src = "images/trash-solid.svg";
+  notesContainer.appendChild(inputBox).appendChild(img);
+});
+
+// Delete Button
+notesContainer.addEventListener("click", (e) => {
+  if (e.target.tagName === "IMG") {
+    e.target.parentElement.remove();
+  }
+});
