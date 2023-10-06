a24cd027c4e61f9eace9e307de06bd6b1247a79c
Ayan Abedin <65453428+ayanabedin1105@users.noreply.github.com>
2023-10-06 01:28:28 +0100

    Color picker added

--- Week_3/problem-2/app.js
+++ Week_3/problem-2/app.js
@@ -1,52 +1,52 @@
-let notesContainer = document.querySelector(".notes-container");
-let createBtn = document.querySelector(".notes-btn");
-let notes = document.querySelectorAll(".input-box");
+// let notesContainer = document.querySelector(".notes-container");
+// let createBtn = document.querySelector(".notes-btn");
+// let notes = document.querySelectorAll(".input-box");
 
-//function to check if localstorage already exists
-function showNotes() {
-  notesContainer.innerHTML = localStorage.getItem("notes");
-}
+// //function to check if localstorage already exists
+// function showNotes() {
+//   notesContainer.innerHTML = localStorage.getItem("notes");
+// }
 
-showNotes();
+// showNotes();
 
-//create Local Storage
-function updateStorage() {
-  localStorage.setItem("notes", notesContainer.innerHTML);
-}
+// //create Local Storage
+// function updateStorage() {
+//   localStorage.setItem("notes", notesContainer.innerHTML);
+// }
 
-updateStorage();
+// updateStorage();
 
 
 
-// creating button
-createBtn.addEventListener("click", () => {
-  let inputBox = document.createElement("p");
-  let img = document.createElement("img");
-  inputBox.className = "input-box";
-  inputBox.setAttribute("contenteditable", "true");
-  img.src = "images/trash-solid.svg";
-  notesContainer.appendChild(inputBox).appendChild(img);
-});
+// // creating button
+// createBtn.addEventListener("click", () => {
+//   let inputBox = document.createElement("p");
+//   let img = document.createElement("img");
+//   inputBox.className = "input-box";
+//   inputBox.setAttribute("contenteditable", "true");
+//   img.src = "images/trash-solid.svg";
+//   notesContainer.appendChild(inputBox).appendChild(img);
+// });
 
-// Delete Button
-notesContainer.addEventListener("click", (e) => {
-  if (e.target.tagName === "IMG") {
-    e.target.parentElement.remove();
-    updateStorage();
-  } else if (e.target.tagName === "P") {
-    notes = document.querySelectorAll(".input-box");
-    notes.forEach((nt) => {
-      nt.onkeyup = function () {
-        updateStorage();
-      };
-    });
-  }
-});
+// // Delete Button
+// notesContainer.addEventListener("click", (e) => {
+//   if (e.target.tagName === "IMG") {
+//     e.target.parentElement.remove();
+//     updateStorage();
+//   } else if (e.target.tagName === "P") {
+//     notes = document.querySelectorAll(".input-box");
+//     notes.forEach((nt) => {
+//       nt.onkeyup = function () {
+//         updateStorage();
+//       };
+//     });
+//   }
+// });
 
-// function to prevent line break
-document.addEventListener("keydown", (e) => {
-  if (e.key == "Enter") {
-    document.execCommand("insertLineBreak");
-    e.preventDefault();
-  }
-});
+// // function to prevent line break
+// document.addEventListener("keydown", (e) => {
+//   if (e.key == "Enter") {
+//     document.execCommand("insertLineBreak");
+//     e.preventDefault();
+//   }
+// });
