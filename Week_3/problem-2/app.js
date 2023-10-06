383e05db950a91a4d243c38c0eb3b90f4dc0587e
Ayan Abedin <65453428+ayanabedin1105@users.noreply.github.com>
2023-10-06 01:08:32 +0100

    Notes app created

--- Week_3/problem-2/app.js
+++ Week_3/problem-2/app.js
@@ -2,6 +2,22 @@ let notesContainer = document.querySelector(".notes-container");
 let createBtn = document.querySelector(".notes-btn");
 let notes = document.querySelectorAll(".input-box");
 
+//function to check if localstorage already exists
+function showNotes() {
+  notesContainer.innerHTML = localStorage.getItem("notes");
+}
+
+showNotes();
+
+//create Local Storage
+function updateStorage() {
+  localStorage.setItem("notes", notesContainer.innerHTML);
+}
+
+updateStorage();
+
+
+
 // creating button
 createBtn.addEventListener("click", () => {
   let inputBox = document.createElement("p");
@@ -16,5 +32,21 @@ createBtn.addEventListener("click", () => {
 notesContainer.addEventListener("click", (e) => {
   if (e.target.tagName === "IMG") {
     e.target.parentElement.remove();
+    updateStorage();
+  } else if (e.target.tagName === "P") {
+    notes = document.querySelectorAll(".input-box");
+    notes.forEach((nt) => {
+      nt.onkeyup = function () {
+        updateStorage();
+      };
+    });
+  }
+});
+
+// function to prevent line break
+document.addEventListener("keydown", (e) => {
+  if (e.key == "Enter") {
+    document.execCommand("insertLineBreak");
+    e.preventDefault();
   }
 });
