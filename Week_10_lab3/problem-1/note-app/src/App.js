9a2d292249838e87d1e8e60647f3740f4519534d
Ayan Abedin <ayanabedin@gmail.com>
2023-11-26 15:05:46 +0000

    Note app created

--- Week_10_lab3/problem-1/note-app/src/App.js
+++ Week_10_lab3/problem-1/note-app/src/App.js
@@ -1,4 +1,3 @@
-import logo from "./logo.svg";
 import React, { useState } from "react";
 import "./App.css";
 
@@ -25,14 +24,30 @@ function App() {
   // variables for Note-App
   const [notes, setNotes] = useState([]);
   const [noteInput, setNoteInput] = useState("");
+  const [selectedColor, setSelectedColor] = useState("#ffffff"); //Default Color
 
   const addNote = () => {
     if (noteInput.trim() !== "") {
-      setNotes([...notes, noteInput]);
+      setNotes([...notes, { text: noteInput, color: selectedColor }]);
       setNoteInput("");
     }
   };
 
+  const changeColor = (color) => {
+    setSelectedColor(color);
+  };
+
+  const deleteNote = (index) => {
+    const updatedNotes = [...notes];
+    updatedNotes.splice(index, 1);
+    setNotes(updatedNotes);
+  };
+
+  const editNote = (index, newText) => {
+    const updatedNotes = [...notes];
+    updatedNotes[index].text = newText;
+    setNotes(updatedNotes);
+  };
   return (
     <div className="App">
       <h1>Note Taking App</h1>
@@ -43,14 +58,60 @@ function App() {
           onChange={(e) => setNoteInput(e.target.value)}
           placeholder="Enter your note"
         />
-
-        <buttton onClick={addNote}>Add Note</buttton>
+        <button onClick={addNote}>Add Note</button>
       </div>
-      <ul>
+
+      <div className="color-selector">
+        <span>Change Note Color:</span>
+        <button
+          onClick={() => changeColor("#ffffff")}
+          style={{ backgroundColor: "#ffffff" }}
+        ></button>
+        <button
+          onClick={() => changeColor("#ffcccb")}
+          style={{ backgroundColor: "#ffcccb" }}
+        ></button>
+        <button
+          onClick={() => changeColor("#ccffcc")}
+          style={{ backgroundColor: "#ccffcc" }}
+        ></button>
+        <button
+          onClick={() => changeColor("#cce6ff")}
+          style={{ backgroundColor: "#cce6ff" }}
+        ></button>
+        <button
+          onClick={() => changeColor("#ce4257")}
+          style={{ backgroundColor: "#ce4257" }}
+        ></button>
+      </div>
+
+      <div className="note-cards">
+        {notes.map((note, index) => (
+          <div
+            key={index}
+            className="note-card"
+            style={{ backgroundColor: note.color }}
+          >
+            {/* <p>{note.text}</p> */}
+            <textarea
+              value={note.text}
+              onChange={(e) => editNote(index, e.target.value)}
+              rows="4"
+              cols="30"
+            />
+
+            <div className="note-actions">
+              <button onClick={() => deleteNote(index)}>Delete</button>
+              <button onClick={() => editNote(index, noteInput)}>Edit</button>
+            </div>
+          </div>
+        ))}
+      </div>
+      {/* <ul>
         {notes.map((note, index) => (
           <li key={index}>{note}</li>
         ))}
-      </ul>
+      </ul> */}
     </div>
   );
 }
