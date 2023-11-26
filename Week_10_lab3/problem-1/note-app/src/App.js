e3134b0c6fe2477ce4d4d934c6e550ca4e4db476
Ayan Abedin <ayanabedin@gmail.com>
2023-11-26 13:25:46 +0000

    React note app created

--- Week_10_lab3/problem-1/note-app/src/App.js
+++ Week_10_lab3/problem-1/note-app/src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  // return (
+  //   <div className="App">
+  //     <header className="App-header">
+  //       <img src={logo} className="App-logo" alt="logo" />
+  //       <p>
+  //         Edit <code>src/App.js</code> and save to reload.
+  //       </p>
+  //       <a
+  //         className="App-link"
+  //         href="https://reactjs.org"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //       >
+  //         Learn React
+  //       </a>
+  //     </header>
+  //   </div>
+  // );
+
+  // variables for Note-App
+  const [notes, setNotes] = useState([]);
+  const [noteInput, setNoteInput] = useState("");
+
+  const addNote = () => {
+    if (noteInput.trim() !== "") {
+      setNotes([...notes, noteInput]);
+      setNoteInput("");
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Note Taking App</h1>
+      <div>
+        <input
+          type="text"
+          value={noteInput}
+          onChange={(e) => setNoteInput(e.target.value)}
+          placeholder="Enter your note"
+        />
+
+        <buttton onClick={addNote}>Add Note</buttton>
+      </div>
+      <ul>
+        {notes.map((note, index) => (
+          <li key={index}>{note}</li>
+        ))}
+      </ul>
     </div>
   );
 }
