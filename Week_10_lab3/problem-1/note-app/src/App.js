1bfac3b0115df06c7bb5630146348b336e8ec35a
Ayan Abedin <ayanabedin@gmail.com>
2023-12-11 21:04:02 +0000

    Lab 3 updated

--- Week_10_lab3/problem-1/note-app/src/App.js
+++ Week_10_lab3/problem-1/note-app/src/App.js
@@ -1,53 +1,84 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import { Subject } from "rxjs";
+import { map } from "rxjs/operators";
 
 function App() {
-  // return (
-  //   <div className="App">
-  //     <header className="App-header">
-  //       <img src={logo} className="App-logo" alt="logo" />
-  //       <p>
-  //         Edit <code>src/App.js</code> and save to reload.
-  //       </p>
-  //       <a
-  //         className="App-link"
-  //         href="https://reactjs.org"
-  //         target="_blank"
-  //         rel="noopener noreferrer"
-  //       >
-  //         Learn React
-  //       </a>
-  //     </header>
-  //   </div>
-  // );
-
-  // variables for Note-App
   const [notes, setNotes] = useState([]);
   const [noteInput, setNoteInput] = useState("");
-  const [selectedColor, setSelectedColor] = useState("#ffffff"); //Default Color
+  const [selectedColor, setSelectedColor] = useState("#ffffff"); // Default color
+  const [editingNoteIndex, setEditingNoteIndex] = useState(null);
+
+  useEffect(() => {
+    const addNote$ = new Subject();
+    const deleteNote$ = new Subject();
+    const editNote$ = new Subject();
+    const colorChange$ = new Subject();
+
+    const subscription = addNote$
+      .pipe(map((note) => [...notes, note]))
+      .subscribe((updatedNotes) => setNotes(updatedNotes));
+
+    const deleteSubscription = deleteNote$
+      .pipe(map((index) => notes.filter((_, i) => i !== index)))
+      .subscribe((updatedNotes) => setNotes(updatedNotes));
+
+    const editSubscription = editNote$
+      .pipe(
+        map(({ index, newText }) =>
+          notes.map((note, i) =>
+            i === index ? { ...note, text: newText } : note
+          )
+        )
+      )
+      .subscribe((updatedNotes) => setNotes(updatedNotes));
+
+    const colorChangeSubscription = colorChange$.subscribe((color) =>
+      setSelectedColor(color)
+    );
+
+    return () => {
+      subscription.unsubscribe();
+      deleteSubscription.unsubscribe();
+      editSubscription.unsubscribe();
+      colorChangeSubscription.unsubscribe();
+    };
+  }, [notes]);
 
   const addNote = () => {
     if (noteInput.trim() !== "") {
-      setNotes([...notes, { text: noteInput, color: selectedColor }]);
+      if (editingNoteIndex !== null) {
+        // If editing, update the existing note
+        editNote(editingNoteIndex, noteInput);
+        setEditingNoteIndex(null);
+      } else {
+        // If not editing, add a new note
+        setNotes([...notes, { text: noteInput, color: selectedColor }]);
+      }
       setNoteInput("");
     }
+  };
+
+  const deleteNote = (index) => {
+    setNotes(notes.filter((_, i) => i !== index));
+  };
+
+  const editNote = (index, newText) => {
+    const updatedNotes = notes.map((note, i) =>
+      i === index ? { ...note, text: newText } : note
+    );
+    setNotes(updatedNotes);
   };
 
   const changeColor = (color) => {
     setSelectedColor(color);
   };
 
-  const deleteNote = (index) => {
-    const updatedNotes = [...notes];
-    updatedNotes.splice(index, 1);
-    setNotes(updatedNotes);
+  const startEditingNote = (index) => {
+    setEditingNoteIndex(index);
+    setNoteInput(notes[index].text);
   };
 
-  const editNote = (index, newText) => {
-    const updatedNotes = [...notes];
-    updatedNotes[index].text = newText;
-    setNotes(updatedNotes);
-  };
   return (
     <div className="App">
       <h1>Note Taking App</h1>
@@ -58,9 +89,10 @@ function App() {
           onChange={(e) => setNoteInput(e.target.value)}
           placeholder="Enter your note"
         />
-        <button onClick={addNote}>Add Note</button>
+        <button onClick={addNote}>
+          {editingNoteIndex !== null ? "Save Edit" : "Add Note"}
+        </button>
       </div>
-
       <div className="color-selector">
         <span>Change Note Color:</span>
         <button
@@ -79,12 +111,7 @@ function App() {
           onClick={() => changeColor("#cce6ff")}
           style={{ backgroundColor: "#cce6ff" }}
         ></button>
-        <button
-          onClick={() => changeColor("#ce4257")}
-          style={{ backgroundColor: "#ce4257" }}
-        ></button>
       </div>
-
       <div className="note-cards">
         {notes.map((note, index) => (
           <div
@@ -92,26 +119,20 @@ function App() {
             className="note-card"
             style={{ backgroundColor: note.color }}
           >
-            {/* <p>{note.text}</p> */}
             <textarea
               value={note.text}
               onChange={(e) => editNote(index, e.target.value)}
               rows="4"
               cols="30"
+              readOnly={editingNoteIndex !== index}
             />
-
             <div className="note-actions">
               <button onClick={() => deleteNote(index)}>Delete</button>
-              <button onClick={() => editNote(index, noteInput)}>Edit</button>
+              <button onClick={() => startEditingNote(index)}>Edit</button>
             </div>
           </div>
         ))}
       </div>
-      {/* <ul>
-        {notes.map((note, index) => (
-          <li key={index}>{note}</li>
-        ))}
-      </ul> */}
     </div>
   );
 }
--- Week_10_lab3/problem-2/timer-app/src/App.js
+++ Week_10_lab3/problem-2/timer-app/src/App.js
@@ -4,32 +4,16 @@ import { map, takeWhile } from "rxjs/operators";
 import "./App.css";
 
 function App() {
-  // return (
-  //   <div className="App">
-  //     <header className="App-header">
-  //       <img src={logo} className="App-logo" alt="logo" />
-  //       <p>
-  //         Edit <code>src/App.js</code> and save to reload.
-  //       </p>
-  //       <a
-  //         className="App-link"
-  //         href="https://reactjs.org"
-  //         target="_blank"
-  //         rel="noopener noreferrer"
-  //       >
-  //         Learn React
-  //       </a>
-  //     </header>
-  //   </div>
-  // );
-
+  const [inputHours, setInputHours] = useState(0);
+  const [inputMinutes, setInputMinutes] = useState(0);
+  const [inputSeconds, setInputSeconds] = useState(0);
   const [hours, setHours] = useState(0);
   const [minutes, setMinutes] = useState(0);
   const [seconds, setSeconds] = useState(0);
   const [timer, setTimer] = useState(null);
 
   const startTimer = () => {
-    const totalSeconds = hours * 3600 + minutes * 60 + seconds;
+    const totalSeconds = inputHours * 3600 + inputMinutes * 60 + inputSeconds;
     const countdown$ = interval(1000).pipe(
       map((i) => totalSeconds - i - 1),
       takeWhile((i) => i >= 0)
@@ -54,32 +38,43 @@ function App() {
     }
   };
 
+  const resetTimer = () => {
+    stopTimer(); // Stop the timer if it's running
+    setInputHours(0);
+    setInputMinutes(0);
+    setInputSeconds(0);
+    setHours(0);
+    setMinutes(0);
+    setSeconds(0);
+  };
+
   return (
     <div className="countdown-container">
       <h2>Countdown Timer</h2>
       <div className="input-fields">
         <input
           type="number"
-          value={hours}
-          onChange={(e) => setHours(e.target.value)}
+          value={inputHours}
+          onChange={(e) => setInputHours(parseInt(e.target.value, 10))}
           placeholder="Hours"
         />
         <input
           type="number"
-          value={minutes}
-          onChange={(e) => setMinutes(e.target.value)}
+          value={inputMinutes}
+          onChange={(e) => setInputMinutes(parseInt(e.target.value, 10))}
           placeholder="Minutes"
         />
         <input
           type="number"
-          value={seconds}
-          onChange={(e) => setSeconds(e.target.value)}
+          value={inputSeconds}
+          onChange={(e) => setInputSeconds(parseInt(e.target.value, 10))}
           placeholder="Seconds"
         />
       </div>
       <div className="timer-buttons">
         <button onClick={startTimer}>START COUNTDOWN</button>
         <button onClick={stopTimer}>STOP</button>
+        <button onClick={resetTimer}>RESET</button>
       </div>
       <div className="timer-output">
         <p>{`${hours.toString().padStart(2, "0")}:${minutes
